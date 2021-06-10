136f270a2a830d25e51d64946fb84bce4e8413af
Diego Urrego <diegourrg@gmail.com>
2021-06-09 23:09:03 -0500

    Se renombran variables de la clase TennisGame3 y se extraen algunos métodos

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,35 +1,54 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int playerTwoPoints;
+    private int playerOnePoints;
+    private String playerOneName;
+    private String playerTwoName;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String score;
+        if (playerOnePoints < 4 && playerTwoPoints < 4 && !(playerOnePoints + playerTwoPoints == 6)) {
+            String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            score = points[playerOnePoints];
+            return tieOrAdventage(score, points);
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            return findWinner();
         }
     }
-    
+
+    private String tieOrAdventage(String score, String[] points) {
+        return (playerOnePoints == playerTwoPoints) ? score + "-All" : score + "-" + points[playerTwoPoints];
+    }
+
+    private String findWinner() {
+        String score;
+        if (playerOnePoints == playerTwoPoints){
+            return "Deuce";
+        }
+
+        score = chosePlayerName();
+        return winner(score);
+    }
+
+    private String winner(String score) {
+        return ((playerOnePoints - playerTwoPoints) * (playerOnePoints - playerTwoPoints) == 1) ? "Advantage " + score : "Win for " + score;
+    }
+
+    private String chosePlayerName() {
+        return playerOnePoints > playerTwoPoints ? playerOneName : playerTwoName;
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.playerOnePoints += 1;
         else
-            this.p2 += 1;
+            this.playerTwoPoints += 1;
         
     }
 
